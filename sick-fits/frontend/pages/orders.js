f71404c076be311104ee0f2a77c2b23d8aa81bfe
Peter Walker <25408515+peteawalk@users.noreply.github.com>
2021-02-08 20:51:26 -0600

    adding pages - next.js style

---
+++ sick-fits/frontend/pages/orders.js
@@ -0,0 +1,5 @@
+export default function OrderPage() {
+    return <div>
+        <p>Hello! Orders Here.</p>
+    </div>
+} 
---
+++ sick-fits/frontend/pages/products.js
@@ -0,0 +1,5 @@
+export default function ProductPage() {
+    return <div>
+        <p>Hello! Products Here.</p>
+    </div>
+} 
